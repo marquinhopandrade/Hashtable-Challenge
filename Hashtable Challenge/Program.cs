8bfb518d20d6fc0cfa6426559db34c139a518df6
Marcos Andrade <mvpdeandrade@hotmail.com>
2023-02-16 17:52:22 -0300

    Adicionar arquivos de projeto.

---
+++ Hashtable Challenge/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Hashtable_Challenge
+{
+    internal class Program
+    {
+        //Challenge
+        /*Write a program taht will iterate through each element of the students array and insert them into a hashtable
+        if a student with the same ID already exists in the hashtable, skip it and siplay the following error:
+        "Sorry, A student with the same ID already Exists".
+        Hint: Use the method ContainsKey() to check wether a student with the same ID already exists*/
+        
+        static void ContainsKey()
+        {
+            Console.WriteLine("Sorry, A student with the same ID already Exists");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
